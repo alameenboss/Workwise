ea5ff3841f4eb0f79ed2962606c7b2b415a5a796
Alameen <4115051+alameenboss@users.noreply.github.com>
2020-01-17 12:24:28 +0530

    Initial Commit

---
+++ Workwise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Workwise.Startup))]
+namespace Workwise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
