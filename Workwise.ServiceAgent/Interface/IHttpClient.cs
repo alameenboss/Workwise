896656bdcc2608fb6a59e197555785c2177c194f
Alameen <4115051+alameenboss@users.noreply.github.com>
2020-02-24 19:56:55 +0530

    Change Name

---
+++ Workwise.ServiceAgent/Interface/IHttpClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http;
+
+namespace Workwise.ServiceAgent
+{
+
+    public interface IHttpClient
+    {
+        HttpResponseMessage PostData<T>(string requestUri, T value);
+        T Get<T>(string requestUri);
+    }
+}
